12b333b99fc07fccc50d632b4366b1e25fb40cc3
David Eastcroft <davideastcroft95@gmail.com>
2020-03-16 17:05:20 +0000

    Initial commit.

---
+++ student.rb
@@ -0,0 +1,31 @@
+class CodeClanStudent
+  def initialize(name, cohort, fav_prog_language)
+    @name = name
+    @cohort = cohort
+    @fav_lang = fav_prog_language
+  end
+
+ #  def get_name
+ #    @name
+ #  end
+ #
+ #  def get_cohort
+ #    @cohort
+ #  end
+ #
+ #  def set_name(name)
+ #    @name = name
+ #  end
+ #
+ #  def set_cohort(cohort)
+ #    @cohort = cohort
+ #  end
+ #
+ #  def talk
+ #    "#{@name} can talk."
+ #  end
+ #
+ #  def say_favourite_language
+ #    "I love #{@fav_lang}"
+ # end
+end
