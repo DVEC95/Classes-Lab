723ff9f99b9e2a88725a2a1c1f07a9d18eaccdac
David Eastcroft <davideastcroft95@gmail.com>
2020-03-16 18:51:56 +0000

    Adds test and function for getting players.

--- specs/sports_team_spec.rb
+++ specs/sports_team_spec.rb
@@ -5,11 +5,15 @@ require_relative('../sports_team.rb')
 
 class TestSportsTeam < MiniTest::Test
   def setup
-    @team_1 = SportsTeam.new("Lakers", ["LeBron James", "Anthony Davis"], "Frank Vogel")
+    @team_1 = SportsTeam.new("Team 1", ["Player 1", "Player 2"], "Coach")
   end
 
   def test_get_team_name
-    assert_equal("Lakers", @team_1.get_team_name)
+    assert_equal("Team 1", @team_1.get_team_name)
+  end
+
+  def test_get_players
+    assert_equal(["Player 1", "Player 2"], @team_1.get_players)
   end
 
 
--- sports_team.rb
+++ sports_team.rb
@@ -1,13 +1,18 @@
 class SportsTeam
   def initialize(team_name, players, coach)
     @team_name = team_name
-    @players = []
+    @players = players
     @coach = coach
   end
 
   def get_team_name
     return @team_name
   end
+
+  def get_players
+    return @players
+  end
+
 
 
 
