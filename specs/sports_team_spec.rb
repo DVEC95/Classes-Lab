4c60c9c6393112ecff5ead5f53da3b6e8ff32a77
David Eastcroft <davideastcroft95@gmail.com>
2020-03-16 20:23:38 +0000

    Refines test and functions.

--- specs/sports_team_spec.rb
+++ specs/sports_team_spec.rb
@@ -7,28 +7,23 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 class TestSportsTeam < MiniTest::Test
   def setup
-    @team_1 = SportsTeam.new("Team 1", ["Player 1", "Player 2"], "Coach")
+    @team_1 = SportsTeam.new('Team 1', ['Player 1', 'Player 2'], 'Coach')
   end
 
   def test_get_team_name
-    assert_equal("Team 1", @team_1.get_team_name)
+    assert_equal('Team 1', @team_1.team_name)
   end
 
   def test_get_players
-    assert_equal(["Player 1", "Player 2"], @team_1.get_players)
+    assert_equal(['Player 1', 'Player 2'], @team_1.players)
   end
 
   def test_get_coach
-    assert_equal("Coach", @team_1.get_coach)
+    assert_equal('Coach', @team_1.coach)
   end
 
   def test_set_coach
-    @team_1.set_coach("New Coach")
-    assert_equal("New Coach", @team_1.get_coach)
+    @team_1.coach = 'New Coach'
+    assert_equal('New Coach', @team_1.coach)
   end
-
-
-
-
-
 end
--- sports_team.rb
+++ sports_team.rb
@@ -1,30 +1,10 @@
 class SportsTeam
+  attr_writer :team_name, :players, :coach
+  attr_reader :team_name, :players, :coach
+
   def initialize(team_name, players, coach)
     @team_name = team_name
     @players = players
     @coach = coach
   end
-
-  def get_team_name
-    return @team_name
-  end
-
-  def get_players
-    return @players
-  end
-
-  def get_coach
-    return @coach
-  end
-
-  def set_coach(new_coach)
-    @coach = new_coach
-  end
-
-
-
-
-
-
-
 end
